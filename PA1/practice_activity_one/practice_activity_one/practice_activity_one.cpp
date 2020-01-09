cc2fe5876d9b95e5c21e0896783216a56267abe3
cooper-a <26753990+cooper-a@users.noreply.github.com>
2020-01-09 15:24:46 -0500

    intial commit pa1

---
+++ PA1/practice_activity_one/practice_activity_one/practice_activity_one.cpp
@@ -0,0 +1,113 @@
+// practice_activity_one.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+class Music {
+public:
+	unsigned int year;
+	string artist_name;
+	string music_id;
+	Music() {
+		year = 0;
+	}
+
+	Music(unsigned int new_year, string new_artist_name, string new_music_id) {
+		year = new_year;
+		artist_name = new_artist_name;
+		music_id = new_music_id;
+	}
+
+	bool operator==(const Music& compare_music) {
+		if (year == compare_music.year && artist_name == compare_music.artist_name && year == compare_music.year) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	string get_artist() {
+		return this->artist_name;
+	}
+
+};
+
+class Song : public Music{
+public:
+	string genre;
+	string song_name;
+	unsigned int song_length;
+
+	Song() {
+		song_length = 0;
+	}
+
+	Song(string new_genre, string new_song_name, unsigned int new_song_length, Music new_music) : genre(new_genre), song_name(new_song_name), song_length(new_song_length), Music(new_music) {}
+
+	bool operator==(const Song& compare_song) {
+		if (song_length == compare_song.song_length && song_name == compare_song.song_name && genre == compare_song.genre) {
+			Music compare_music = static_cast<Music>(compare_song);
+			return compare_music == static_cast<Music>(*this);
+		}
+		else {
+			return false;
+		}
+	}
+};
+
+class Playlist {
+	friend Playlist operator+(const Playlist& playlist_one, const Playlist& playlist_two);
+
+public:
+	vector<Song> my_playlist;
+
+	bool insert_song(Song& song_info) {
+		string artist = song_info.get_artist();
+		int artist_count = 0;
+		bool can_insert = true;
+		for (Song song : my_playlist){
+			if (song == song_info) {
+				can_insert = false;
+			}
+			if (artist == song.get_artist()) {
+				++artist_count;
+			}
+		}
+		can_insert == can_insert && (artist_count > 3);
+		if (can_insert) {
+			my_playlist.push_back(song_info);
+		}
+		return can_insert;
+	}
+
+
+	Playlist shuffle_songs() {
+
+	}
+
+};
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
